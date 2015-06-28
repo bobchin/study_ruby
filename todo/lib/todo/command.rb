c7f67c538eb40c682ea4e6e967ff07a51a87b2c5
bobchin <bobchin.ryu@gmail.com>
2015-06-28 16:50:48 +0900

    15.4.3

--- todo/lib/todo/command.rb
+++ todo/lib/todo/command.rb
@@ -2,9 +2,19 @@
 
 module Todo
 
-# コマンドラインベースの処理をするクラス
-# @author bobchin
-class Command
-end
+  # コマンドラインベースの処理をするクラス
+  # @author bobchin
+  class Command
+
+    def execute
+      DB.prepare
+    end
+
+    def create_task(name, content)
+      # タスクの作成時の status はデフォルト値が使われNOT_YETとなる
+      Task.create!(name: name, content: content).reload
+    end
+
+  end
 
 end
