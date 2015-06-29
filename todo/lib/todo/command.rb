18603a75f942e408a3ca511ed62ada043a2a9090
bobchin <bobchin.ryu@gmail.com>
2015-06-30 00:15:38 +0900

    15.6

--- todo/lib/todo/command.rb
+++ todo/lib/todo/command.rb
@@ -24,16 +24,16 @@ module Todo
       DB.prepare
 
       tasks = case sub_command
-      when 'create'
-        create_task(options[:name], options[:content])
-      when 'delete'
-        delete_task(options[:id])
-      when 'update'
-        update_task(options.delete(:id), options)
-      when 'list'
-        find_tasks(options[:status])
+        when 'create'
+          create_task(options[:name], options[:content])
+        when 'delete'
+          delete_task(options[:id])
+        when 'update'
+          update_task(options.delete(:id), options)
+        when 'list'
+          find_tasks(options[:status])
       end
-      p tasks
+      display_tasks tasks
     rescue => e
       abort "Error: #{e.message}"
     end
@@ -68,6 +68,28 @@ module Todo
       else
         all_tasks
       end
+    end
+
+    private
+
+    def display_tasks(tasks)
+      header = display_format('ID', 'Name', 'Content', 'Status')
+      puts header
+
+      puts '-' * header.size
+      Array(tasks).each do |task|
+        puts display_format(task.id, task.name, task.content, task.status_name)
+      end
+    end
+
+    def display_format(id, name, content, status)
+      name_length = 20 - full_width_count(name)
+      content_length = 40 - full_width_count(content)
+      [id.to_s.rjust(4), name.ljust(name_length), content.ljust(content_length), status.center(10)].join(' | ')
+    end
+
+    def full_width_count(string)
+      string.each_char.select {|char| !(/[ -~。-]/.match(char))}.count
     end
   end
 
